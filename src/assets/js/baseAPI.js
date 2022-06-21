0748548da7a70d0fadeb544de9ff2dd6c32f72ca
dawei <dawei17629375839@163.com>
2022-06-21 17:52:11 +0800

    完成个人中心功能的开发

--- src/assets/js/baseAPI.js
+++ src/assets/js/baseAPI.js
@@ -1,22 +1,29 @@
-//注意每次调用 $.get(),$.post()或者$.ajax()的时候，都会调用$.ajaxPrefilter（）这个函数，
-//在这个函数中我们可以拿到我们给ajax提供的配置对象
-$.ajaxPrefilter((option) =>{
-//在发起真正的ajax请求之前，统一拼接的根路径
-option.url ='http://www.liulongbin.top:3007'+ option.url
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function(options) {
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  options.url = 'http://www.liulongbin.top:3007' + options.url
 
-//统一为有权限的接口,设置headers请求头
-if(option.url.indexOf('/my/') !==-1){
-  option.headers={
-    Authorization:localStorage.getItem('token') || ''
-}
+  // 统一为有权限的接口，设置 headers 请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
 
-}
-//全局统一挂载complete
-option.complete = function(res){
-//console.log(res)
-    if(res.responseJSON.status ===1 && res.responseJSON.message === '身份认证失败!'){
+
+
+  // 全局统一挂载 complete 回调函数
+  options.complete = function(res) {
+    // console.log('执行了 complete 回调：')
+    // console.log(res)
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 1. 强制清空 token
       localStorage.removeItem('token')
-      location.href ='/login.html'
+      // 2. 强制跳转到登录页面
+      location.href = '/login.html'
     }
   }
 
